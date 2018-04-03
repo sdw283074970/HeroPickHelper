223c79b16cd56249c5a8cbb5b74153ac7d438957
sdw283074970 <downwes@gmail.com>
2018-04-02 22:02:03 -0700

    建立前端布局

---
+++ HeroPickHelper/Controllers/HeroCalculatorController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HeroPickHelper.Controllers
+{
+    public class HeroCalculatorController : Controller
+    {
+        // GET: HeroCalculator
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
