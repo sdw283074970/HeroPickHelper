944d04344d2d9378cbec2c9d4bb9bf6fb22cdb61
sdw283074970 <downwes@gmail.com>
2018-04-03 17:54:06 -0700

    重构了api布局，更改了POST请求的返回类型，现在返回类型为Created

--- HeroPickHelper/Controllers/Api/HeroCalculatorController.cs
+++ HeroPickHelper/Controllers/Api/HeroCalculatorController.cs
@@ -47,30 +47,22 @@ namespace HeroPickHelper.Controllers.Api
         [HttpPost]
         public IHttpActionResult GetCalculateResult(int[] enemyIds)
         {
-            ////查找HeroDuty列表中含有DutyId为1/2/3/4/5的对象(这个对象包含了英雄Id的信息)
-            //var carriesInfo = GetDutyInfo(1);
-            //var midsInfo = GetDutyInfo(2);
-            //var offlanesInfo = GetDutyInfo(3);
-            //var roamAndJunglesInfo = GetDutyInfo(4);
-            //var supportsInfo = GetDutyInfo(5);
-
-            ////返回这个计算结果列表给前端
-            //var resultList = new ResultList()
-            //{
-            //    Carries = carriesInfo,
-            //    Mids = midsInfo,
-            //    RoamOrJuggles = roamAndJunglesInfo,
-            //    Offlanes = offlanesInfo,
-            //    Supports =supportsInfo
-            //};
-
+            //实例化helper对象，该函数封装了核心算法和各种辅助函数
             var helper = new HeroDutyHelper();
 
+            //如果ModeState没被验证，则返回BadRequest
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+
+            //如果收到的json数据包为空，则返回BadRequest
             if (enemyIds == null || enemyIds.Count() > 5)
             {
                 return BadRequest();
             }
 
+            //如果json数组中只有一个元素(英雄)，则省略合并步骤
             if (enemyIds.Count() == 1)
             {
                 var enemyCounteredHeroList = helper.GetEnemyCounteredHeroList(enemyIds[0]);
@@ -78,15 +70,16 @@ namespace HeroPickHelper.Controllers.Api
 
                 var resultList = helper.GetCalculateResult(weightedList);
 
-                return Ok(resultList);
+                return Created(Request.RequestUri + "/" + enemyIds[0],resultList);
             }
-            else
+            else        //否则，调用封装在helper对象中的合并列表函数
             {
                 var weightedList = new List<HeroCounter>();
                 for (int i = 0; i < enemyIds.Count(); i++)
                 {
                     var enemyCounteredHeroList = helper.GetEnemyCounteredHeroList(enemyIds[i]);
                     var currentWeightedList = helper.GetWeightedList(enemyCounteredHeroList, enemyIds[i]).ToList();
+                    //挨个合并列表
                     if (i == 0)
                     {
                         weightedList = currentWeightedList;
@@ -98,9 +91,10 @@ namespace HeroPickHelper.Controllers.Api
 
                 }
 
+                //输出结果
                 var resultList = helper.GetCalculateResult(weightedList);
 
-                return Ok(resultList);
+                return Created(Request.RequestUri + "/" + enemyIds[0] + ":" + enemyIds.Last(), resultList);
             }
         }
     }
